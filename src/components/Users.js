cceaeda14e122bc74a4f452487f027cbd1be4213
zahid2022slipq <zahid.ali.skipq@gmail.com>
2023-01-02 15:47:37 +0500

    input form added

--- src/components/Users.js
+++ src/components/Users.js
@@ -14,9 +14,12 @@ const Users = () => {
   };
 
   return (
-    <div>
+    <div className="flex justify-center mt-4">
       {data.map((d) => (
-        <div className="bg-slate-300 p-3 mb-3">
+        <div className="flex flex-col bg-slate-300 p-3 mb-3">
+          <div className="pl-5 font-bold">
+            <button className="border-2">X</button>
+          </div>
           <div>
             <h2>Name</h2>
           </div>
@@ -25,8 +28,30 @@ const Users = () => {
           </div>
         </div>
       ))}
+      <div className="bg-slate-300 ml-3 p-4">
+        <div>
+          <button
+            className="bg-green-300 p-1 rounded-full mb-8"
+            onClick={getData}
+          >
+            Get Data
+          </button>
+        </div>
 
-      <button onClick={getData}>Get Data</button>
+        <div className="mb-2">
+          <input className="p-2 rounded-md" placeholder="name" />
+        </div>
+
+        <div className="mb-2">
+          <input className="p-2 rounded-md" placeholder="interests" />
+        </div>
+
+        <div>
+          <button className="bg-sky-600 p-4 rounded-md" onClick={getData}>
+            Save User
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
